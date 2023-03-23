7512b2716269caabebaa54123a78488b00b04f72
Lari <larileal@hotmail.com.br>
2023-03-23 16:53:38 -0300

    feat: adding the data model

---
+++ Back/APILIVROS/APILIVROS/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APILIVROS.Models;
+
+public class Book
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title can't be empty")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Gender can't be empty")]
+    [StringLength(50, ErrorMessage = "Gender accept till 50 characters")]
+    public string Gender { get; set; }
+
+    [Required(ErrorMessage = "Author can't be empty")]
+    public string Author { get; set; }
+
+    [Required(ErrorMessage = "Pages can't be empty")]
+    public string Pages { get; set; }
+
+    [Required(ErrorMessage = "Publishing House can't be empty")]
+    public string PublishingHouse { get; set; }
+}
