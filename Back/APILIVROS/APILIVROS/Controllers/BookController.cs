cd74256651b6d65b3b6f2b1b34e18fece6cd7031
Lari <larileal@hotmail.com.br>
2023-03-23 21:00:30 -0300

    feat: adding the CRUD methods

--- Back/APILIVROS/APILIVROS/Controllers/BookController.cs
+++ Back/APILIVROS/APILIVROS/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using APILIVROS.Data;
 using APILIVROS.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APILIVROS.Controllers;
@@ -21,4 +22,49 @@ public class BookController : ControllerBase
         _context.SaveChanges();
         return CreatedAtAction(nameof(CreateBook), new { id = book.Id }, book);
     }
+
+    [HttpGet]
+    public IEnumerable<Book> GetAllBooks([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return _context.Books.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetBookById(int id)
+    {
+        var book = _context.Books.FirstOrDefault(book => book.Id == id);
+        if (book == null) return NotFound();
+        return Ok(book);
+    }
+
+
+    [HttpPatch("{id}")]
+    public IActionResult UpdateBookById(int id, JsonPatchDocument<Book> patch)
+    {
+        var book = _context.Books.FirstOrDefault(book => book.Id == id);
+        if (book == null) return NotFound();
+
+        patch.ApplyTo(book, ModelState);
+
+        if (!TryValidateModel(book))
+        {
+            return ValidationProblem(ModelState);
+        }
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBook(int id)
+    {
+        var book = _context.Books.FirstOrDefault(book => book.Id == id);
+        if (book == null) return NotFound();
+        _context.Remove(book);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+
+
 }
